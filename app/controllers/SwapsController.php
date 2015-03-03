f9e2d477b6e0f3436c13f79c538da4c3a15bd8e7
Sascha Wagner <wagners@kph.uni-mainz.de>
2015-03-03 12:56:10 +0100

    Controller for swap requests added - alpha

---
+++ app/controllers/SwapsController.php
@@ -0,0 +1,200 @@
+<?php
+
+class SwapsController extends \BaseController {
+
+	protected $swap;
+
+	public function __construct(Swap $swap)
+	{
+		$this->swap = $swap;
+	}
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//TODO: maybe show a list of all existing swap requests?
+	}
+
+
+	/**
+	 * Display the beamtime with swap option.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function create($id)
+	{
+		$shift = Shift::find($id);
+		$beamtime_id = $shift->beamtime->id;
+		if ($beamtime = Beamtime::find($beamtime_id))
+			$shifts = $beamtime->shifts;
+		else
+			return 'Beamtime not found!';
+
+		return View::make('beamtimes.swap')->with('beamtime', $beamtime)->with('shifts', $shifts)->with('current', $id);
+	}
+
+
+	/**
+	 * Create the swap request by storing an entry in the database sending emails to the users of this shift
+	 *
+	 * @param int $shift_org_id, int $shift_req_id
+	 * @return Response
+	 */
+	public function store($org, $req)
+	{
+		$shift_org = Shift::find($org);
+		$shift_request = Shift::find($req);
+		$beamtime_id = $shift_request->beamtime->id;
+		if ($beamtime_id != $shift_org->beamtime->id)
+			return Redirect::back()->with('error', 'The shifts don\'t belong to the same beamtime, please use the interface corretly!');
+		if (!$shift_org->users->find(Auth::id())->count())
+			return Redirect::back()->with('error', 'You\'re not subscribed to the original shift, please use the interface corretly!');
+
+		// the query seems reasonable, create an entry in the database for this request and send emails to the users of this shift
+		$swap = new Swap();
+		$hash = $swap->create_hash($beamtime_id, $org, $req);
+
+		// create database entry
+		$swap->user_id = Auth::id();
+		$swap->hash = $hash;
+		$swap->original_shift_id = $org;
+		$swap->request_shift_id = $req;
+		//$swap->save();
+		//IMPORTANT: only mail swap requests to possible users, i. e. not to the user who is already subscribed to the original shift
+//TODO: test mail transmission!
+echo 'Users on shift: <br />';
+$shift_request->users->each(function($user){ print $user->username . '<br />'; });
+echo '<br />';
+echo 'User who is on the original shift:<br />';
+$other_user_org = $shift_org->get_other_user(Auth::id());
+if ($other_user_org)
+	print $other_user_org->username . '<br />';
+echo '<br />';
+echo 'Users who should receive an email:<br />';
+if ($other_user_org)
+	$users = $shift_request->users->filter(function($user) use($other_user_org)
+	{
+		return $user->id != $other_user_org->id;
+	});
+else
+	$users = $shift_request->users;
+$to = array();
+$users->each(function($user) use($to, &$to)
+{
+	$to[] = $user->email;
+	print 'Mail request to '. $user->username . ' (' . $user->email . ')<br />';
+});
+echo '<br />';
+
+/*echo Request::root();
+echo '<br />';
+echo url();
+echo '<br />';
+echo asset('/');
+echo '<br />';
+echo URL::to('/'); //App::make('url')->to('/');
+echo '<br />';
+echo Request::getHost();
+echo '<br />';
+echo Request::getHttpHost();
+echo '<br />';
+echo $_SERVER['HTTP_HOST'];
+echo '<br />';
+echo $_SERVER['SERVER_NAME'];
+echo '<br />';*/
+
+$subject = 'Swap Request from ' . Auth::user()->get_full_name();
+$msg = 'Hello,\r\n\r\n';
+$msg.= Auth::user()->first_name . ' wants to swap shifts. ' . Auth::user()->first_name . ' is assigned to the shift on ' . date("l, jS F Y, \s\\t\a\\r\\t\i\\n\g \a\\t H:i", strtotime($shift_org->start)) . ' and wants to change to your shift on ' . date("l, jS F Y, \s\\t\a\\r\\t\i\\n\g \a\\t H:i", strtotime($shift_request->start)) . '.\r\n';
+$msg.= 'You can view the swap request for the related beamtime in detail here: ' . Request::root() . '/swaps/' . $hash . '\r\n\r\n';
+$msg.= 'A2 Beamtime Scheduler';
+
+echo '<br />#######################<br />';
+echo 'Mail content: <br /><br />';
+echo 'To: ' . implode(', ', $to) . '<br />';
+echo 'Subject: ' . $subject;
+echo '<br /><br />';
+echo str_replace(array('\r\n'), array('<br />'), $msg);
+echo '<br />';
+echo '<br />#######################<br />';
+
+	}
+
+
+	/**
+	 * Display the current swap request with the BeamtimesController
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		if (!$swap = $this->swap->whereHash($id)->first())
+			return Redirect::to('')->with('warning', 'This swap request is not available. It may have already been performed.');
+
+		$beamtime = Beamtime::find(Shift::find($swap->original_shift_id)->beamtime->id);
+		$shifts = $beamtime->shifts;
+
+		return View::make('beamtimes.swap')->with('beamtime', $beamtime)->with('shifts', $shifts)
+					->with('swap', $id)->with('org', $swap->original_shift_id)->with('req', $swap->request_shift_id);
+	}
+
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		if (!$swap = $this->swap->whereHash($id)->first())
+			return Redirect::to('')->with('warning', 'This swap request is not available. It may have already been performed.');
+
+		$shift_org = Shift::find($swap->original_shift_id);
+		$shift_req = Shift::find($swap->request_shift_id);
+		$user_id = $swap->user_id;
+
+		// swap the users
+		$shift_org->users()->detach($user_id);
+		$shift_org->users()->attach(Auth::id());
+		$shift_req->users()->detach(Auth::id());
+		$shift_req->users()->attach($user_id);
+
+		// delete the swap request
+		$swap->delete();
+
+		return Redirect::to('beamtimes/' . $shift_org->beamtime->id)->with('success', 'Shift workers swapped successfully.');
+	}
+
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+
+}
