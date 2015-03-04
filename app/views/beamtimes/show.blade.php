f46108f1ed5f7555c5836a8afc1fe6eba98ed364
Sascha Wagner <wagners@kph.uni-mainz.de>
2015-03-04 13:01:04 +0100

    Edit button for admins in beamtime view added

--- app/views/beamtimes/show.blade.php
+++ app/views/beamtimes/show.blade.php
@@ -16,7 +16,15 @@ $(document).ready(function() {
 @section('content')
 <div class="col-lg-10 col-lg-offset-1">
     <div class="page-header">
+      @if (Auth::user()->isAdmin)
+      <table width="100%"><tr><td>
         <h2>Beamtime: {{{ $beamtime->name }}}</h2>
+      </td><td class="text-right hidden-print">
+        <a class="btn btn-primary btn" href="/beamtimes/{{{$beamtime->id}}}/edit"><span class="fa fa-pencil"></span>&nbsp;&nbsp;&nbsp;Edit Beamtime</a>
+      </td></tr></table>
+      @else
+      <h2>Beamtime: {{{ $beamtime->name }}}</h2>
+      @endif
     </div>
 
     {{-- Check if the beamtime contain shifts to avoid errors --}}
