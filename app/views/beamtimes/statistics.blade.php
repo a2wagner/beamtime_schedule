1d2d1db22183d0d45ef5da6af9dbd94b8cea5ab4
Sascha Wagner <wagners@kph.uni-mainz.de>
2015-03-10 11:05:56 +0100

    Current state, use the new UtilityCollection

--- app/views/beamtimes/statistics.blade.php
+++ app/views/beamtimes/statistics.blade.php
@@ -9,11 +9,9 @@
 <div class="col-lg-10 col-lg-offset-1">
 Total beamtimes: {{{ Beamtime::all()->count() }}} <br />
 <?php
-//use Illuminate\Support\Collection;
-use Illuminate\Database\Eloquent\Collection;
-
 $year = 2015;
 //$year = array(2014, 2015, 2016);
+
 $beamtimes = Beamtime::all()
 	->filter(function($beamtime) use($year)
 	{
@@ -32,23 +30,25 @@ $hours = $beamtimes->sum(function($beamtime)
 		return $beamtime->shifts->sum('duration');
 	});
 
-$shifts = new Collection();
-$beamtimes->each(function($beamtime) use(&$shifts)
-	{
-		$shifts->merge($beamtime->shifts);
-	});
-
-echo '<h2>Statistics for ' . $year . "</h2>\n";
-echo $beamtimes->count() . ' beamtimes with ' . $beamtimes->sum(function($beamtime){ return $beamtime->shifts->count(); }) . " shifts<br />\n";
-echo 'Total beamtime: '. $hours . ' hours (' . round($hours/24, 1) . " days)<br />\n";
-
-
-}
+#$beamtimes->each(function($beamtime) use(&$shifts)
+#	{
+#		$shifts->merge($beamtime->shifts);
+#	});
 
  // --> $results = $articles->groupBy('category');
 
-
-//DB::table('name')->pluck('column')
-//Beamtime::where(DB::raw('YEAR(created_at)'), '=', date('n') )->get()
 ?>
+
+<div class="page-header">
+    <h2>Statistics for {{{ $year }}}</h2>
 </div>
+
+{{{ $beamtimes->count() }}} beamtimes with {{{ $beamtimes->shifts->count() }}} shifts<br />
+Total beamtime: {{{ $hours }}} hours ({{{ round($hours/24, 1) }}} days)<br />
+
+<?php
+}
+?>
+
+</div>
+@stop
