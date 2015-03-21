78ebb95740d5ad4c8ef578f077c408acb58c1753
Sascha Wagner <wagners@kph.uni-mainz.de>
2015-03-21 17:02:27 +0100

    Added determination of shift type

--- app/models/RCShift.php
+++ app/models/RCShift.php
@@ -16,6 +16,20 @@ class RCShift extends Eloquent
 	 */
 	const START = 8;
 
+	/**
+	 * General time (hour of day) to check if a given shift is a day shift
+	 *
+	 * @var int
+	 */
+	const DAY = 14;
+
+	/**
+	 * General time (hour of day) to check if a given shift is a night shift
+	 *
+	 * @var int
+	 */
+	const NIGHT = 2;
+
 	// Do not use timestamps for this model
 	public $timestamps = false;
 
@@ -64,9 +78,80 @@ class RCShift extends Eloquent
 	 */
 	protected $table = 'rc_shifts';
 
+	/**
+	* Check if a given date is between to other dates
+	*
+	* @param $start, $end, $date
+	* @return boolean
+	*/
+	public static function date_in_range($start, $end, $date = 'now')
+	{
+		// convert non-timestamps to timestamps
+		$start = is_int($start) || $start instanceof DateTime ? $start : strtotime($start);
+		$end = is_int($end) || $start instanceof DateTime ? $end : strtotime($end);
+		$date = is_int($date) || $start instanceof DateTime ? $date : strtotime($date);
+
+		// check if start and end order is correct
+		if ($start > $end)
+			return $this->date_in_range($end, $start, $date);
+
+		// check if the date is between start and end date
+		return $date > $start && $date < $end;
+	}
+
+	/**
+	* Check if the run coordinator shift is a day or night shift
+	*
+	* @return string
+	*/
 	public function type()
 	{
-		//TODO
-		return 'day or night';
+		if ($this->is_day())
+			return 'day';
+		else
+			return 'night';
+	}
+
+	/**
+	* Check if the shift is during a specific time ($hour)
+	*
+	* @param int $hour
+	* @return boolean
+	*/
+	public function check_time($hour)
+	{
+		if (!is_int($hour))
+			return false;
+
+		$start = new DateTime($this->start);
+		$end = clone($start);
+		$dur = 'PT' . $this->duration . 'H';
+		$end->add(new DateInterval($dur));
+		$date = clone($start);
+		$date->setTime($hour, 00);
+		if ($date < $start)
+			$date->modify('+1 day');
+
+		return $this->date_in_range($start, $end, $date);
+	}
+
+	/**
+	* Check if the shift is a day shift
+	*
+	* @return boolean
+	*/
+	public function is_day()
+	{
+		return $this->check_time(self::DAY);
+	}
+
+	/**
+	* Check if the shift is a night shift
+	*
+	* @return boolean
+	*/
+	public function is_night()
+	{
+		return $this->check_time(self::NIGHT);
 	}
 }
--- app/models/Shift.php
+++ app/models/Shift.php
@@ -1,6 +1,27 @@
 <?php
 
 class Shift extends \Eloquent {
+	/**
+	 * General time (hour of day) to check if a given shift is a day shift
+	 *
+	 * @var int
+	 */
+	const DAY = 12;
+
+	/**
+	 * General time (hour of day) to check if a given shift is a day shift
+	 *
+	 * @var int
+	 */
+	const LATE = 20;
+
+	/**
+	 * General time (hour of day) to check if a given shift is a night shift
+	 *
+	 * @var int
+	 */
+	const NIGHT = 4;
+
 	protected $fillable = ['beamtime_id', 'start', 'duration', 'n_crew', 'maintenance', 'remark'];
 
 	// Do not use timestamps for this model
@@ -81,5 +102,94 @@ class Shift extends \Eloquent {
 	{
 		$date = new DateTime($this->start);
 		return $date->format('Y') == $year;
+	}
+
+	/**
+	* Check if a given date is between to other dates
+	*
+	* @param $start, $end, $date
+	* @return boolean
+	*/
+	public static function date_in_range($start, $end, $date = 'now')
+	{
+		// convert non-timestamps to timestamps
+		$start = is_int($start) || $start instanceof DateTime ? $start : strtotime($start);
+		$end = is_int($end) || $start instanceof DateTime ? $end : strtotime($end);
+		$date = is_int($date) || $start instanceof DateTime ? $date : strtotime($date);
+
+		// check if start and end order is correct
+		if ($start > $end)
+			return $this->date_in_range($end, $start, $date);
+
+		// check if the date is between start and end date
+		return $date > $start && $date < $end;
+	}
+
+	/**
+	* Check if the current shift is a day, late or night shift
+	*
+	* @return string
+	*/
+	public function type()
+	{
+		if ($this->is_day())
+			return 'day';
+		elseif ($this->is_late())
+			return 'late';
+		else
+			return 'night';
+	}
+
+	/**
+	* Check if the shift is during a specific time ($hour)
+	*
+	* @param int $hour
+	* @return boolean
+	*/
+	public function check_time($hour)
+	{
+		if (!is_int($hour))
+			return false;
+
+		$start = new DateTime($this->start);
+		$end = clone($start);
+		$dur = 'PT' . $this->duration . 'H';
+		$end->add(new DateInterval($dur));
+		$date = clone($start);
+		$date->setTime($hour, 00);
+		if ($date < $start)
+			$date->modify('+1 day');
+
+		return $this->date_in_range($start, $end, $date);
+	}
+
+	/**
+	* Check if the shift is a day shift
+	*
+	* @return boolean
+	*/
+	public function is_day()
+	{
+		return $this->check_time(self::DAY);
+	}
+
+	/**
+	* Check if the shift is a late shift
+	*
+	* @return boolean
+	*/
+	public function is_late()
+	{
+		return $this->check_time(self::LATE);
+	}
+
+	/**
+	* Check if the shift is a night shift
+	*
+	* @return boolean
+	*/
+	public function is_night()
+	{
+		return $this->check_time(self::NIGHT);
 	}
 }
