49e00a4a9f0db96f7e838f1ab3f4edf9bfa0d7dc
Sascha Wagner <wagners@kph.uni-mainz.de>
2016-02-08 11:02:11 +0100

    Add radiation instruction link to admin's users menu

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -70,6 +70,7 @@
                           <ul class="dropdown-menu">
                             <li><a href="/users"><i class="fa fa-bars fa-fw"></i> All Users</a></li>
                             <li><a href="/users/enable"><i class="fa fa-check-square-o fa-fw"></i> Enable Users</a></li>
+                            <li><a href="/users/radiation"><i class="fa">&thinsp;&#9762;&nbsp;</i> Radiation Instruction</a></li>
                             <li><a href="/users/manage"><i class="fa fa-wrench fa-fw"></i> Manage Users</a></li>
                           </ul>
                         </li>
@@ -81,7 +82,7 @@
                           <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-users fa-fw"></i> Users <span class="fa fa-caret-down"></span></a>
                           <ul class="dropdown-menu">
                             <li><a href="/users"><i class="fa fa-bars fa-fw"></i> All Users</a></li>
-                            <li><a href="/users/radiation"><i class="fa">&nbsp;&#9762;&nbsp;</i> Radiation Instruction</a></li>
+                            <li><a href="/users/radiation"><i class="fa">&thinsp;&#9762;&nbsp;</i> Radiation Instruction</a></li>
                           </ul>
                         </li>
                         @else
