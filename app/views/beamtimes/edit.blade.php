9e3c54ac2a3126a9f37b7fb233987c7d2cc693f0
Sascha Wagner <wagners@kph.uni-mainz.de>
2016-04-28 14:16:53 +0200

    Adapt shift status to new rating levels

--- app/views/beamtimes/edit.blade.php
+++ app/views/beamtimes/edit.blade.php
@@ -172,8 +172,8 @@ function toggleRadio(id)
             </div>
           </td>
           {{ $td }}@if ($shift->users->count() == 0) <a href="#" class="btn btn-danger btn-sm disabled">Empty</a>
-          @elseif ($shift->users->sum('rating') < 5 ) <a href="#" class="btn btn-warning btn-sm disabled">Bad</a>
-          @elseif ($shift->users->sum('rating') < 8 ) <a href="#" class="btn btn-primary btn-sm disabled">Good</a>
+          @elseif ($shift->users->sum('rating') < 4 ) <a href="#" class="btn btn-warning btn-sm disabled">Bad</a>
+          @elseif ($shift->users->sum('rating') < 6 ) <a href="#" class="btn btn-primary btn-sm disabled">Good</a>
           @else <a href="#" class="btn btn-success btn-sm disabled">Perfect</a>
           @endif</td>
           {{ $td }}</td>
