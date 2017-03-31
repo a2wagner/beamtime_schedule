508e1f12f28387f38d6423a1278f9043a7dacdd7
Sascha Wagner <wagners@kph.uni-mainz.de>
2017-03-31 06:50:14 +0200

    Inlcude const values and method checks for workgroup regions

--- app/models/Workgroup.php
+++ app/models/Workgroup.php
@@ -7,6 +7,27 @@ class Workgroup extends \Eloquent {
 	public $timestamps = false;
 
 	/**
+	 * region int used for local group(s)
+	 *
+	 * @var int
+	 */
+	const LOCAL = 0;
+
+	/**
+	 * region int used for european groups
+	 *
+	 * @var int
+	 */
+	const EUROPE = 1;
+
+	/**
+	 * region int used for non-european groups
+	 *
+	 * @var int
+	 */
+	const WORLD = 2;
+
+	/**
 	* A workgroup has many members (users); one to many relation
 	*
 	* @return Array of User models
@@ -14,5 +35,49 @@ class Workgroup extends \Eloquent {
 	public function members()
 	{
 		return $this->hasMany('User');
+	}
+
+	/**
+	* Checks if a workgroup is local
+	*
+	* @return bool
+	*/
+	public function local()
+	{
+		return $this->region === self::LOCAL;
+	}
+
+	/**
+	* Checks if a workgroup is european
+	*
+	* @return bool
+	*/
+	public function europe()
+	{
+		return $this->region === self::EUROPE;
+	}
+
+	/**
+	* Checks if a workgroup is non-european
+	*
+	* @return bool
+	*/
+	public function world()
+	{
+		return $this->region === self::WORLD;
+	}
+
+	/**
+	* Get string of the current workgroup region
+	*
+	* @return string
+	*/
+	public function region()
+	{
+		if ($this->local())
+			return 'Local';
+		if ($this->europe())
+			return 'Europe';
+		return 'Outside Europe';
 	}
 }
